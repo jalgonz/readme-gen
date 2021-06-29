b46aa112f2cc07ddd24634f76973c68a650c2e02
Joshua Gonzalez <jalgonz96@gmail.com>
2021-06-29 13:57:01 -0400

    updated renderLicenseSection function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -38,7 +38,11 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  licenseBadge = renderLicenseBadge(data.license);
+  licenseLink = renderLicenseLink(data.license);
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
